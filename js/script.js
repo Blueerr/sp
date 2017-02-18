7211e4a7eecf88c9393bb1398393a59ff1b66f9a
Blueerr <salvogiova01@hotmail.com>
2017-02-18 11:07:37 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ function init() {
   initSearchBar();
   buildDate();
   buildMenu();
-  $('body').style.opacity = 0
+  $('body').style.opacity = 1;
   $('mainContainer').style.opacity = 1;
   $('dateContainer').style.opacity = 1;
   $('notesWidget').style.opacity = 1;
